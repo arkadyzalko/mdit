639b11439ae5212500e045ba56218ecd4f81a18f
arkadyzalko <arkady.zalkowitsch@commure.com>
2026-07-10 00:23:56 -0700

    feat(editor): scaffold heading minimap export

---
+++ packages/editor/src/minimap/index.ts
@@ -0,0 +1,1 @@
+export { HeadingMinimap, type HeadingMinimapProps } from "./minimap"
---
+++ packages/editor/src/minimap/minimap.tsx
@@ -0,0 +1,8 @@
+export type HeadingMinimapProps = {
+	topOffset?: number
+	className?: string
+}
+
+export function HeadingMinimap(_props: HeadingMinimapProps) {
+	return null
+}
